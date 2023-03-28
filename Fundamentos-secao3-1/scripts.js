cabb7fd4786a5123768151fa92fa25913091c905
IanPedroBorges <ianhudback@gmail.com>
2023-03-28 15:54:53 -0300

    operador &&

--- Fundamentos-secao3-1/scripts.js
+++ Fundamentos-secao3-1/scripts.js
@@ -1,3 +1,4 @@
+/*
 var nota = 100;
 if (nota >= 80){
     console.log('Parabéns, você faz parte do grupo de pessoas aprovadas')
@@ -5,4 +6,21 @@ if (nota >= 80){
     console.log('Você está na nossa lista de espera')
 }else if (nota < 60){
     console.log('Infelizmente, você reprovou')
+}*/
+
+let currentHour = 22;
+let message ='';
+if (currentHour >= 22){
+    message = 'Não deveriamos comer nada , é hora de dormir'
+}else if (currentHour >= 18 && currentHour < 22){
+    message = 'Rango da noite, vamos jantar :D'
+}else if (currentHour >= 14 && currentHour < 18){
+    message = 'Vamos fazer um bolo pro café da tarde?'
+}else if (currentHour >= 11 && currentHour < 14){
+    message = 'Hora do almoço!!!'
+}else if (currentHour >= 4 && currentHour < 11){
+    message = 'Hmmm, cheiro de café recém-passado'
+}else if (currentHour >= 0 && currentHour < 4){
+    message = 'Ta de madruga , vai dormir'
 }
+console.log(message)
