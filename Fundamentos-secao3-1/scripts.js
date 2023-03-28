02d4cd4da99dcd050ec77072461d52a3ce505488
IanPedroBorges <ianhudback@gmail.com>
2023-03-28 16:12:02 -0300

    terminando o swicth

--- Fundamentos-secao3-1/scripts.js
+++ Fundamentos-secao3-1/scripts.js
@@ -6,7 +6,7 @@ if (nota >= 80){
     console.log('Você está na nossa lista de espera')
 }else if (nota < 60){
     console.log('Infelizmente, você reprovou')
-}*/
+}
 
 let currentHour = 22;
 let message ='';
@@ -24,3 +24,27 @@ if (currentHour >= 22){
     message = 'Ta de madruga , vai dormir'
 }
 console.log(message)
+
+let weekDay = 'sabado';
+if (weekDay == 'segunda-feira' || weekDay == 'terça-feira' || weekDay == 'quarta-feira' || weekDay == 'quinta-feira' || weekDay == 'sexta-feira'){
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D')
+}else if(weekDay == 'sabado' || weekDay == 'Domingo'){
+    console.log('FINALMENTE, descanso merecido! UwU!')
+}
+*/
+let status = 'Barabam'
+switch (status) {
+    case 'aprovada':
+        console.log('Parabéns, você está no grupo de pessoas aprovadas!');
+        break;
+    case 'lista':
+        console.log('Você está na nossa lista de espera;');
+        break;
+    case 'reprovada':
+        console.log('Infelizmente, você reprovou');
+        break;
+
+    default:
+        console.log('Informação Incorreta')
+        break;
+}
