7cd567150957939a2c80ddd1d851af4ccff6162a
IanPedroBorges <ianhudback@gmail.com>
2023-04-10 15:21:10 -0300

    terminando as atv antes da aula ao vivo

---
+++ Fundamentos-secao-4/scripts.js
@@ -0,0 +1,13 @@
+const urgente = document.querySelectorAll('.emergency-tasks h3');
+urgente[0].style.backgroundColor = 'purple';
+urgente[1].style.backgroundColor = 'purple';
+
+const divUrgente = document.querySelectorAll('.emergency-tasks');
+divUrgente[0].style.backgroundColor = 'Salmon';
+
+const naoUrgente = document.querySelectorAll('.no-emergency-tasks h3');
+naoUrgente[0].style.backgroundColor = 'black';
+naoUrgente[1].style.backgroundColor = 'black';
+
+const divNaoUrgente = document.querySelectorAll('.no-emergency-tasks');
+divNaoUrgente[0].style.backgroundColor = 'Khaki'
